7cc96267a2113b4cf62ab63bce38c585309b4cf7
b0bland <alfredychan96@gmail.com>
2019-06-06 21:25:34 -0700

    Added functionality to grab GIFs from API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
 
+$("button").on("click", function () { 
+event.preventDefault();
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Vw9epddThJnMcOqUCvmhP6DvvalODALI"
+
+
+var search = "Juventus"
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Vw9epddThJnMcOqUCvmhP6DvvalODALI&q=" + search + "&limit=10"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    var gifs = response.data
+    for (var i=0;i<gifs.length;i++) {
+        var teams = gifs[i].images.original.url
+        $("#images").append("<img src='" + teams + "'>")
+    }
+    
+})
+
+
+})
